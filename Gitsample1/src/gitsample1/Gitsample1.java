e3492ae64eee457b1fea91c9d1732e77524d6110
ejanas <m3e.janas@me.com>
2015-05-22 09:57:53 +0200

    Second checkin

--- Gitsample1/src/gitsample1/Gitsample1.java
+++ Gitsample1/src/gitsample1/Gitsample1.java
@@ -15,7 +15,8 @@ public class Gitsample1 {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    System.out.println("Hello");
+    // Test
+    System.out.println("Hello world");
   }
 
 }
