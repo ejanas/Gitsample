96f4b99fffacb0627ca623dc6b7ae61cfe03383e
ejanas <m3e.janas@me.com>
2015-05-22 14:42:56 +0200

    Change to 4

--- Gitsample1/src/gitsample1/Gitsample1.java
+++ Gitsample1/src/gitsample1/Gitsample1.java
@@ -16,7 +16,7 @@ public class Gitsample1 {
    */
   public static void main(String[] args) {
     // Test
-    System.out.println("Hello world 3");
+    System.out.println("Hello world 4");
   }
 
 }
