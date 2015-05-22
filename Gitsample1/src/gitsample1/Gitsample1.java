098d930dd2e544af782b57a0cc00cfb2e456c499
ejanas <m3e.janas@me.com>
2015-05-22 13:32:32 +0200

    Changed back to Hello1

--- Gitsample1/src/gitsample1/Gitsample1.java
+++ Gitsample1/src/gitsample1/Gitsample1.java
@@ -16,7 +16,7 @@ public class Gitsample1 {
    */
   public static void main(String[] args) {
     // Test
-    System.out.println("Hello world2");
+    System.out.println("Hello world1");
   }
 
 }
